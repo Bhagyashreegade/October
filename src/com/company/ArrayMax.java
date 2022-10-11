57eab38c320cef2d914becda1af5fd6d9de1c567
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-10-11 13:15:37 -0400

    a added

--- src/com/company/ArrayMax.java
+++ src/com/company/ArrayMax.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class ArrayMax {
     public static void main(String[] args) {
-        System.out.println("Array is Main Class");
+        System.out.println("Array is a Main Class");
 
         int [] number= {10,50,60,120,100};
 
