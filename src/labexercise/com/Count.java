86f7e19d7b35121fefb172e2a568249f995c1a69
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-10-11 01:44:52 -0400

    counts added

--- src/labexercise/com/Count.java
+++ src/labexercise/com/Count.java
@@ -4,7 +4,19 @@ public class Count {
 
     public static void main(String[] args) {
 
-        //Count
+        // Count,
+        // Bhagyashree,
+        // The program counts from 1 to 5 in English, French and Spanish.
+
+        // In English
+        System.out.println("One two three four five");
+
+        //In French
+        System.out.println("un deux trois quarte cinq");
+
+        //In Spanish----> after removing one slash --error--illegal state of expression
+        System.out.println("uno dos tres cuarto cinco");
+
 
 
 
