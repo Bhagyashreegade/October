d73735b19d106183a612b03acccc9c4f5e6ab8c5
Bhagyashree Gade <bhagyashreegade91@gmail.com>
2022-10-17 13:33:30 -0400

    second approach for finding duplicates in ArrayList

--- src/com/company/ListDuplicateRemoval.java
+++ src/com/company/ListDuplicateRemoval.java
@@ -12,25 +12,38 @@ public class ListDuplicateRemoval {
         list.add("Urvi");
         list.add("Bhagya");
 
-        //The values tha t are duplicated
+        //1---->The values that are duplicated
 
-        for (int i = 0; i < list.size(); i++) {
+        /*for (int i = 0; i < list.size(); i++) {
             if (list.lastIndexOf(list.get(i)) != i) {
                 System.out.println(list.get(i) + " is duplicated");
             }
+        }*/
+
+        //2----> how to print duplicates in ArrayList
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++)
+                if (list.get(i) == list.get(j)) {
+                    System.out.println("found duplicate element :" + list.get(i));
+                }
         }
 
-        //how many times each duplicate is present
+        // How many times each duplicate is present
 
 
         System.out.println(Collections.frequency(list, "Bhagya"));
         System.out.println(Collections.frequency(list, "Urvi"));
 
-        //remove duplicate from ArrayList--> By converting it into Set
-
-        System.out.println("Before removing the duplicate: " + list);
+        //To check if it has duplicate values & remove duplicate from ArrayList--> By converting it into Set
 
         Set<String> s = new HashSet<String>(list);
+        if (s.size() == list.size()) {
+            System.out.println("The list doesn't have duplicate values");
+        } else {
+            System.out.println("The list has a duplicate value");
+        }
+
+        System.out.println("Before removing the duplicate: " + list);
 
         System.out.println("After removing the duplicates: " + s);
 
